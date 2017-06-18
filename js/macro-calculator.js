19f90c5e9b6480adc9bd5fc0ae8219af310da0d8
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-06-17 23:25:42 -0700

    first commit

---
+++ js/macro-calculator.js
@@ -0,0 +1,59 @@
+
+
+/*
+ If < 40% bodyfat
+	 Protein = 1.2 * LBM 
+ else 
+	Protein = 45% tdee
+ 
+ Fat = .3 * LBM ( )
+*/
+
+
+function calculateMacro(macroType) {
+		
+	var data = calculateBodyFat();
+	if(data){
+	
+	switch(macroType) {
+		case (macroType == "protein") :
+		
+
+		var proteinPercent = $(".deficit-macros input[name=='protein']").val(); 		// get protein input
+		
+	
+		break;
+		
+		case (macroType == "fat") :
+	
+		break;
+		
+		case (macroType == "carbs") :
+
+		break;				
+		
+	}// end switch
+	
+	}
+	
+}// end calculateMacro()
+
+
+function calculateFat() { 
+
+}// end calculateFat
+
+
+function calculateCarbs() { 
+
+}// end calculateCarbs
+
+
+
+
+function updateMacros() {
+	
+	
+	
+}// end updateMacros
+
